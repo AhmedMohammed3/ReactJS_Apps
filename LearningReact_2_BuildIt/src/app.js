d6e373d6013bf387b1fae54c22a006448d3debd5
Ahmed Mohamed Hassan <ahm.moh.has@gmail.com>
2021-07-25 14:26:08 +0200

    Added a baseline for new max course

--- LearningReact_2_BuildIt/src/app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import AppRouter from './routers/app.router';
-// import 'normalize.css/normalize.css'
-import './styles/styles.scss';
-
-
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
