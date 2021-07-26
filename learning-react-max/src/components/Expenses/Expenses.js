af16c3abea7fb38249baf031d36b78aaff5e4ffb
Ahmed Mohamed Hassan <ahm.moh.has@gmail.com>
2021-07-26 02:26:41 +0200

    Finished Section 5

--- learning-react-max/src/components/Expenses/Expenses.js
+++ learning-react-max/src/components/Expenses/Expenses.js
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem/ExpenseItem'
 import Card from '../UI/Card/Card';
 import ExpensesFilter from './ExpensesFilter/ExpensesFilter';
 import './Expenses.css';
+import ExpensesList from './ExpensesList/ExpensesList';
+import ExpensesChart from './ExpensesChart/ExpensesChart';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -11,30 +12,19 @@ const Expenses = (props) => {
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
-    <Card className="expenses">
-    <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
-    </Card>
+    <div>
+      <ExpensesChart expenses={filteredExpenses} />
+      <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
   )
 };
 
--- learning-react-max/src/components/NewExpense/NewExpense.js
+++ learning-react-max/src/components/NewExpense/NewExpense.js
@@ -1,20 +1,42 @@
 import { v4 as uuid } from 'uuid';
+import { useState } from 'react';
+
 import ExpenseForm from './ExpenseForm/ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
-    const saveExpenseDataHandler = (enteredExpenseData) => {
-        const expenseData = {
-            ...enteredExpenseData,
-            id: uuid().toString()
-        }
-        props.onAddExpense(expenseData)
-    }
-    return (
-        <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-        </div>
-    );
-}
+  const [isEditing, setIsEditing] = useState(false);
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: uuid().toString(),
+    };
+    props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
+  return (
+    <div className='new-expense'>
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
+    </div>
+  );
+};
 
 export default NewExpense;
